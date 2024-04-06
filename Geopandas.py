295331209936d679b14cefc3e1c2b1564a5d8b1a
ZMBG50 <znicholsoncox@gmail.com>
2024-04-06 19:30:32 +0100

    # Households Plot
    Finished Plot of # Households using pandas and Geopandas.

--- Geopandas.py
+++ Geopandas.py
@@ -1,8 +1,20 @@
-
-
 import geopandas
+import pandas as pd 
+
+#https://www.ons.gov.uk/peoplepopulationandcommunity/wellbeing/articles/subnationalindicatorsexplorer/2022-01-06
+
+df = pd.read_csv("pcd_p002.csv")
+df['Postcode'] = df['Postcode'].str.split(' ').str[0]
+df['Postcode'] = df['Postcode'].replace('\d', '', regex=True)
+df = df.rename(columns={"Postcode":"id"})
+df = df.groupby('id').sum().reset_index()
+
+print(df)
 
 UK = geopandas.read_file("uk-postcode-area.geojson")
+
+UK = pd.merge(right=df, left=UK, on="id")
+
 
 #uk-postcode-area.geojson
 
@@ -10,8 +22,11 @@ print(UK)
 
 UK.crs
 
-UK = UK.set_geometry("geometry")
-UK2 = UK.to_crs("EPSG:4326")
-UK.plot(legend=True)
+#UK = UK.set_geometry("geometry")
+#UK2 = UK.to_crs("EPSG:4326")
 
+UK.plot(column='Count', cmap='viridis', legend=True)
+#UK.plot(legend=False)
+
+print(UK)
 ###i am testing the commit

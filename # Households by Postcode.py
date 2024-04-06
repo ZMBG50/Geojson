5a07b8615609cac19499ee82b39ae16c049a7a7a
ZMBG50 <znicholsoncox@gmail.com>
2024-04-06 20:00:41 +0100

    Renamed File

--- # Households by Postcode.py
+++ # Households by Postcode.py
@@ -3,24 +3,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-#https://www.nomisweb.co.uk/sources/census_2021_pc
+#Source of data: https://www.nomisweb.co.uk/sources/census_2021_pc
 
+#Data transformation
 df = pd.read_csv("Household #.csv")
 df['Postcode'] = df['Postcode'].str.split(' ').str[0]
 df['Postcode'] = df['Postcode'].replace('\d', '', regex=True)
 df = df.rename(columns={"Postcode":"id"})
 df = df.groupby('id').sum().reset_index()
 
-print(df)
-
+#loading geojson
 UK = geopandas.read_file("uk-postcode-area.geojson")
 
+#merging csv data with geojson
 UK = pd.merge(right=df, left=UK, on="id")
 
-print(UK)
-
-#UK.crs
-
+#plotting data
 fig, ax = plt.subplots(1, 1)
 divider = make_axes_locatable(ax)
 cax = divider.append_axes("right", size="5%", pad=0.1)
@@ -32,6 +30,5 @@ UK_plot = UK.plot(column='Count',
                   legend=True,
                   cax=cax,
                   legend_kwds={"orientation": "vertical"})
-
 UK_plot.set_axis_off()
 ax.set_title(title1)
